5082a3f618e2c9c5d2200da59342089e57e9b363
RJHCarto <richard.hargreaves@live.co.uk>
2018-05-08 22:19:27 -0400

    Final Commits 1

--- ucesrh1/www/js/formActivity.js
+++ ucesrh1/www/js/formActivity.js
@@ -1,12 +1,36 @@
-// load the map
-var mymap = L.map('mapid').setView([51.524428, -0.134224], 17);
-				// load the tiles
-		L.tileLayer('https://api.mapbox.com/styles/v1/rjhargreaves/cjcv3d22i04bp2rpza7tjx1c4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmpoYXJncmVhdmVzIiwiYSI6ImNpa3JmbDJiazAwMDF3Y20xMHoyaXowdDAifQ.78vWSemMDwn42TwMuxfODw', {maxZoom: 18,attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    var northstar = L.tileLayer('https://api.mapbox.com/styles/v1/rjhargreaves/cjcv3d22i04bp2rpza7tjx1c4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmpoYXJncmVhdmVzIiwiYSI6ImNpa3JmbDJiazAwMDF3Y20xMHoyaXowdDAifQ.78vWSemMDwn42TwMuxfODw', {maxZoom: 18,attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' 
       + 
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
       id: 'mapbox.streets'
-		}).addTo(mymap);
+    
+    }),
+
+    scenic   = L.tileLayer('https://api.mapbox.com/styles/v1/rjhargreaves/cjgxxe0ra003t2rnrkjvu8ofs/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmpoYXJncmVhdmVzIiwiYSI6ImNpa3JmbDJiazAwMDF3Y20xMHoyaXowdDAifQ.78vWSemMDwn42TwMuxfODw', {maxZoom: 18,attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' 
+      + 
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    
+    });
+  // load the map
+    var mymap = L.map('mapid',{
+      center: [51.524428, -0.134224], 
+      zoom: 13,
+      layers: [scenic, northstar]
+    });
+
+    var baseMaps = {
+      "Scenic": scenic,
+      "Plain": northstar
+    };
+    // load the tiles
+    L.control.layers(baseMaps).addTo(mymap);
+
+var testMarkerGray = L.AwesomeMarkers.icon({
+      icon: 'play',
+      markerColor: 'lightgray'
+    });
 
 var marker = L.marker([51.524428, -0.134224], {
     draggable:true
--- ucesrh1/www/js/uploadData.js
+++ ucesrh1/www/js/uploadData.js
@@ -1,3 +1,52 @@
+    // create the code to get the questions data using an XMLHttpRequest
+    function getQuestions() {
+      client = new XMLHttpRequest();
+
+    client.open('GET','http://developer.cege.ucl.ac.uk:30290/getGeoJSON/quizlet/geom');
+      client.onreadystatechange = questionResponse; 
+      // note don't use questionResponse() with brackets as that doesn't work
+      client.send();
+    }
+    // create the code to wait for the response from the data server, and process the response once it is received
+    function questionResponse() {
+    // this function listens out for the server to say that the data is ready - i.e. has state 4
+    if (client.readyState == 4) {
+      // once the data is ready, process the data
+      var questiondata = client.responseText;
+      loadquestionlayer(questiondata);
+      }
+    }
+
+
+ 
+    var app_array = [];
+    // convert the received data - which is text - to JSON format and add it to the map
+    function loadquestionlayer(questiondata) {
+      // convert the text received from the server to JSON
+      var questionjson = JSON.parse(questiondata);
+
+      // load the geoJSON layer
+      var questionlayer = L.geoJson(questionjson,
+      {
+
+
+        onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.question+'<div> <form id="Qform" style= "text-align:center" > <input type="radio" name="answer" id=check1 value="one" checked>'+feature.properties.answerone+ '<br> <input type="radio" name="answer" id=check2 value="two">'+feature.properties.answertwo+ '<br> <input type="radio" name="answer" id=check3 value="three">'+feature.properties.answerthree+ '<br> <input type="radio" name="answer" id=check4 value="four">' + feature.properties.answerfour +'<br></form></div>');
+
+  }, 
+
+        // use point to layer to create the points
+        pointToLayer: function (feature, latlng)
+        {
+        quiz_marker = L.marker(latlng, {icon:testMarkerGray});
+        app_array.push(quiz_marker);
+        return quiz_marker
+        
+    },
+  }).addTo(mymap);
+mymap.fitBounds(questionlayer.getBounds()); 
+} 
+
 function startDataUpload() {
 	var Question = document.getElementById("Question").value;
 	var AnswerOne = document.getElementById("AnswerOne").value;
